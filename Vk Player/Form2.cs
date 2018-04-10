3f133ba64d176244725091a62c0f73b639670fa2
Alex Gorban <alex.cheys@gmail.com>
2018-04-10 16:38:21 +0300

    Добавил форму авторизации

---
+++ Vk Player/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vk_Player
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=3649985&scope=audio&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.74&response_type=token");
+        }
+    }
+}
